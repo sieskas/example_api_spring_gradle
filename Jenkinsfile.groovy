eabd3de5741ffa06babcde475d4e085a48857b70
rogodard <sieskas@gmail.com>
2023-02-12 17:51:27 -0500

    add sonar in pipline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,6 +12,12 @@ pipeline {
         bat './gradlew build'
       }
     }
+    stage('SonarQube analysis') {
+      def scannerHome = tool 'sonar';
+      withSonarQubeEnv('My SonarQube Server') { // If you have configured more than one global server connection, you can specify its name
+        sh "${scannerHome}/bin/sonar-scanner"
+      }
+    }
   }
   post {
     always {
