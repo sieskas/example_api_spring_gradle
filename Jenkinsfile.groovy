e70248c5162d2d9028e93e8153181a29d846897e
rogodard <sieskas@gmail.com>
2023-02-12 18:08:47 -0500

    modify sonar pipline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,37 +1,32 @@
 pipeline {
-  agent any
-  tools {
-    gradle 'gradle'
-  }
-  options {
-    buildDiscarder(logRotator(numToKeepStr: '5'))
-  }
-  stages {
-
-    stage('Git Checkout') {
-      steps {
-        //enable remote triggers
-        script {
-          properties([pipelineTriggers([pollSCM('''* * * * *''')])])
+    agent any
+    tools {
+        gradle 'gradle'
+    }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5'))
+    }
+    stages {
+        stage('build') {
+            steps {
+                bat './gradlew build'
+            }
         }
-        git credentialsId: 'Jenkins', url: 'https://github.com/sieskas/example_api_spring_gradle'
-      }
+        stage('SonarQube analysis') {
+            steps {
+                script {
+                    // requires SonarQube Scanner 2.8+
+                    scannerHome = tool 'sonar'
+                }
+                withSonarQubeEnv('SonarQube Scanner') {
+                    sh "${scannerHome}/bin/sonar-scanner"
+                }
+            }
+        }
     }
-    stage('build') {
-      steps {
-        bat './gradlew build'
-      }
+    post {
+        always {
+            junit '**/*.xml'
+        }
     }
-    stage('SonarQube analysis') {
-      def scannerHome = tool 'sonar';
-      withSonarQubeEnv('My SonarQube Server') { // If you have configured more than one global server connection, you can specify its name
-        sh "${scannerHome}/bin/sonar-scanner"
-      }
-    }
-  }
-  post {
-    always {
-      junit '**/*.xml'
-    }
-  }
 }
